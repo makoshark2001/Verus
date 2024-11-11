dd88fd11330633ad4538b5f255d274b5ce3c3503
HTMauney <makoshark2001@gmail.com>
2024-11-11 16:29:24 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,9 +15,6 @@ echo "Downloading latest release: $GITHUB_DOWNLOAD_NAME"
 
 wget ${GITHUB_DOWNLOAD_URL} -O ~/ccminer/ccminer
 wget https://raw.githubusercontent.com/makoshark2001/Verus/main/config.json -O ~/ccminer/config.json
-wget https://raw.githubusercontent.com/makoshark2001/Verus/main/config_verushash.json -O ~/ccminer/config_verushash.json
-wget https://raw.githubusercontent.com/makoshark2001/Verus/main/config_sha256d.json -O ~/ccminer/config_sha256d.json
-wget https://raw.githubusercontent.com/makoshark2001/Verus/main/config_scrypt.json -O ~/ccminer/config_scrypt.json
 chmod +x ~/ccminer/ccminer
 
 cat << EOF > ~/ccminer/start.sh
@@ -25,41 +22,10 @@ cat << EOF > ~/ccminer/start.sh
 ~/ccminer/ccminer -c ~/ccminer/config.json
 EOF
 
-cat << EOF > ~/ccminer/start_verushash.sh
-#!/bin/sh
-~/ccminer/ccminer -c ~/ccminer/config_verushash.json
-EOF
-
-cat << EOF > ~/ccminer/start_sha256d.sh
-#!/bin/sh
-~/ccminer/ccminer -c ~/ccminer/config_sha256d.json
-EOF
-
-cat << EOF > ~/ccminer/start_scrypt.sh
-#!/bin/sh
-~/ccminer/ccminer -c ~/ccminer/config_scrypt.json
-EOF
-
-
 
 cat << EOF > ~/ccminer/start-screen.sh
 #!/bin/sh
 screen -S Verus -d -m ./start.sh
-EOF
-
-cat << EOF > ~/ccminer/start-screen-verushash.sh
-#!/bin/sh
-screen -S Verus -d -m ./start_verushash.sh
-EOF
-
-cat << EOF > ~/ccminer/start-screen-sha256d.sh
-#!/bin/sh
-screen -S Bitcoin -d -m ./start_sha256d.sh
-EOF
-
-cat << EOF > ~/ccminer/start-screen-scrypt.sh
-#!/bin/sh
-screen -S Scrypt -d -m ./start_scrypt.sh
 EOF
 
 
